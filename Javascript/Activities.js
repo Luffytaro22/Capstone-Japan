a62d37c22eacdc18ef44133d1f2de9f769cea67c
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-05-19 21:19:49 -0500

    Saved the information of the activities in an object

---
+++ Javascript/Activities.js
@@ -0,0 +1,40 @@
+const container = document.querySelector('#activities-container');
+
+const activities = [
+    {
+        activity: 'Gastronomy',
+        type: 'Sushi, Ramen, Takoyaki, Okonomiyaki, Tempura, Soba, Udon, Oden, Onigiri',
+        description: 'The gastronomy of Japan as a national cuisine has evolved, through the centuries, due to many political, social and resource changes, starting from the Ancient Age, in which most of the cuisine was influenced by Chinese culture.',
+        img: 'Images/japanese-food.jpg',
+    },
+    {
+        activity: 'Writing',
+        type: 'Kanji, Hiragana, Katakana, Romaji',
+        description: 'The Japanese script derives from the Chinese script, which was brought to Japan via Korea in the fourth century.',
+        img: 'Images/japanese-letter.jpg',
+    },
+    {
+        activity: 'Outfit',
+        type: 'Yukata, Kimono, Junihitoe, Keikogi, Jinbe',
+        description: 'Daily clothing in Japan is like typical Western clothing, pants and shirts are not new. Some outstanding clothing of traditional clothing can be said to be common in daily life.',
+        img: 'Images/japanese-clothes.jpg',
+    },
+    {
+        activity: 'Tea Ceremony',
+        type: '抹茶, 茶の湯, 茶事',
+        description: 'The Japanese tea ceremony is a ritual way of preparing green or matcha tea, influenced by Zen Buddhism, served to a small group of guests in a tranquil setting.',
+        img: 'Images/japanese-tea.jpg',
+    },
+    {
+        activity: 'Architecture',
+        type: '日本建築',
+        description: 'Mostly, the buildings that have survived to this day and that show characteristics of traditional Japanese architecture are castles, Buddhist temples, and Shinto shrines.',
+        img: 'Images/japanese-architecture.jpg',
+    },
+    {
+        activity: 'Hanami',
+        type: '花見',
+        description: 'Hanami is the Japanese tradition of observing the beauty of flowers, but this word is generally associated with the period when the cherry blossoms are in bloom and when the Japanese flock to parks and gardens to gaze at their blossoms (sakura).',
+        img: 'Images/japanese-hanami.jpg',
+    }
+];
