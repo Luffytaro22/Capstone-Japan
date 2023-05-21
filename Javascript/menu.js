580dce5e24c2a7438448d80153e82bb1da9a01ba
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-05-21 09:42:49 -0500

    Format files

--- Javascript/menu.js
+++ Javascript/menu.js
@@ -3,11 +3,11 @@ const hamburguer = document.querySelector('#hamburger-container');
 const x = document.querySelector('#x-container');
 
 function showMenu() {
-  menuContainer.style.display = 'flex';
+	menuContainer.style.display = 'flex';
 }
 
 function closeMenu() {
-  menuContainer.style.display = 'none';
+	menuContainer.style.display = 'none';
 }
 
 hamburguer.addEventListener('click', showMenu);
