475cd6f196ac6ddf7acfa305f14e2037661b6cce
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-05-24 10:30:17 -0500

    Added a position fixed to the body when the mobile menu appears

--- Javascript/menu.js
+++ Javascript/menu.js
@@ -1,13 +1,16 @@
 const menuContainer = document.querySelector('#mobile-menu');
 const hamburguer = document.querySelector('#hamburger-container');
 const x = document.querySelector('#x-container');
+const body = document.body;
 
 function showMenu() {
   menuContainer.style.display = 'flex';
+  body.style.position = 'fixed';
 }
 
 function closeMenu() {
   menuContainer.style.display = 'none';
+  body.style.position = 'unset';
 }
 
 hamburguer.addEventListener('click', showMenu);
